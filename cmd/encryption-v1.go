0ad6977761e60e066ed3bb65ba0a803c4a89ce25
Harshavardhana <harsha@minio.io>
2018-02-23 18:20:01 -0800

    Use regular DecryptRequest when no parts are provided

--- cmd/encryption-v1.go
+++ cmd/encryption-v1.go
@@ -499,7 +499,14 @@ func (w *DecryptBlocksWriter) Close() error {
 // DecryptBlocksRequest - setup a struct which can decrypt many concatenated encrypted data
 // parts information helps to know the boundaries of each encrypted data block.
 func DecryptBlocksRequest(client io.Writer, r *http.Request, startOffset, length int64, srcInfo ObjectInfo) (io.WriteCloser, int64, int64, error) {
-	_, encStartOffset, encLength := getStartOffset(startOffset, length)
+	seqNumber, encStartOffset, encLength := getStartOffset(startOffset, length)
+	if len(srcInfo.Parts) == 0 {
+		writer, err := DecryptRequestWithSequenceNumber(client, r, seqNumber, srcInfo.UserDefined)
+		if err != nil {
+			return nil, 0, 0, err
+		}
+		return writer, encStartOffset, encLength, nil
+	}
 
 	var partStartIndex int
 	var partStartOffset int64
