9e67e5a278c77ccd25af79386bfcd01ffea39c7e
Anis Elleuch <anis@min.io>
2021-01-01 21:01:33 +0100

    Accelerate parent is object

--- cmd/erasure-common.go
+++ cmd/erasure-common.go
@@ -18,8 +18,8 @@ package cmd
 
 import (
 	"context"
-	"path"
 	"sort"
+	"strings"
 	"sync"
 
 	"github.com/minio/minio/pkg/sync/errgroup"
@@ -206,25 +206,30 @@ func (er erasureObjects) getLoadBalancedDisks(optimized bool) []StorageAPI {
 // This function does the following check, suppose
 // object is "a/b/c/d", stat makes sure that objects ""a/b/c""
 // "a/b" and "a" do not exist.
-func (er erasureObjects) parentDirIsObject(ctx context.Context, bucket, parent string) bool {
-	var isParentDirObject func(string) bool
-	isParentDirObject = func(p string) bool {
-		if p == "." || p == SlashSeparator {
+func (er erasureObjects) parentDirIsObject(ctx context.Context, bucket, parent string) (ret bool) {
+	segments := strings.Split(parent, slashSeparator)
+	if segments[0] == "" {
+		return false
+	}
+	path := ""
+	for _, s := range segments {
+		path += s
+		isObject, doesNotExist := er.isObject(ctx, bucket, path)
+		if doesNotExist {
 			return false
 		}
-		if er.isObject(ctx, bucket, p) {
+		if isObject {
 			// If there is already a file at prefix "p", return true.
 			return true
 		}
-		// Check if there is a file as one of the parent paths.
-		return isParentDirObject(path.Dir(p))
+		path += "/"
 	}
-	return isParentDirObject(parent)
+	return false
 }
 
 // isObject - returns `true` if the prefix is an object i.e if
 // `xl.meta` exists at the leaf, false otherwise.
-func (er erasureObjects) isObject(ctx context.Context, bucket, prefix string) (ok bool) {
+func (er erasureObjects) isObject(ctx context.Context, bucket, prefix string) (ok, pathDoesNotExist bool) {
 	storageDisks := er.getDisks()
 
 	g := errgroup.WithNErrs(len(storageDisks))
@@ -246,5 +251,6 @@ func (er erasureObjects) isObject(ctx context.Context, bucket, prefix string) (o
 	// ignored if necessary.
 	readQuorum := getReadQuorum(len(storageDisks))
 
-	return reduceReadQuorumErrs(ctx, g.Wait(), objectOpIgnoredErrs, readQuorum) == nil
+	err := reduceReadQuorumErrs(ctx, g.Wait(), objectOpIgnoredErrs, readQuorum)
+	return err == nil, err == errPathNotFound
 }
