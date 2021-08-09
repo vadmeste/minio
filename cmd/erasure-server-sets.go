5681e2838ebe140b1f36e0baa13e967316d09412
Anis Elleuch <vadmeste@users.noreply.github.com>
2021-08-09 14:58:34 +0100

    Always deduplicate folders in listing (#12909)
    When a prefix with a trailing slash is found, always deduplicate it.
    
    Co-authored-by: Anis Elleuch <anis@min.io>

--- cmd/erasure-server-sets.go
+++ cmd/erasure-server-sets.go
@@ -1006,6 +1006,12 @@ func lexicallySortedEntryZone(zoneEntryChs [][]FileInfoCh, zoneEntries [][]FileI
 				continue
 			}
 
+			// Always deduplicate prefixes
+			if lentry.Name == zoneEntries[i][j].Name && strings.HasSuffix(lentry.Name, slashSeparator) {
+				lexicallySortedEntryCount++
+				continue
+			}
+
 			// Entries are duplicated across disks,
 			// we should simply skip such entries.
 			if lentry.Name == zoneEntries[i][j].Name && lentry.ModTime.Equal(zoneEntries[i][j].ModTime) && setIndex == zoneEntryChs[i][j].SetIndex {
@@ -1097,6 +1103,12 @@ func lexicallySortedEntryZoneVersions(zoneEntryChs [][]FileInfoVersionsCh, zoneE
 	for i, entriesValid := range zoneEntriesValid {
 		for j, valid := range entriesValid {
 			if !valid {
+				continue
+			}
+
+			// Always deduplicate prefixes
+			if lentry.Name == zoneEntries[i][j].Name && strings.HasSuffix(lentry.Name, slashSeparator) {
+				lexicallySortedEntryCount++
 				continue
 			}
 
