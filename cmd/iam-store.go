527d232be2d0aa7a5665d1223e0f85a39f06a7e5
Aditya Manthramurthy <aditya@minio.io>
2024-06-24 12:20:51 -0700

    Add IAM (re)load timing logs
    This is useful to debug large IAM load times - the usual cause is when
    there are a large amount of temporary accounts.

--- cmd/iam-store.go
+++ cmd/iam-store.go
@@ -562,7 +562,7 @@ func (store *IAMStoreSys) LoadIAMCache(ctx context.Context, firstTime bool) erro
 			return err
 		}
 	} else {
-
+		// Only non-object IAM store (i.e. only etcd backend).
 		bootstrapTraceMsg("loading policy documents")
 		if err := store.loadPolicyDocs(ctx, newCache.iamPolicyDocsMap); err != nil {
 			return err
