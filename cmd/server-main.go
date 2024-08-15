792775fe7df037ffeeefa032507ed3d2fd5cb78e
Harshavardhana <harsha@minio.io>
2024-08-14 17:34:56 -0700

    avoid crash when initializing bucket quota cache (#20258)

--- cmd/server-main.go
+++ cmd/server-main.go
@@ -1084,11 +1084,6 @@ func serverMain(ctx *cli.Context) {
 			globalSiteReplicationSys.Init(GlobalContext, newObject)
 		})
 
-		// Initialize quota manager.
-		bootstrapTrace("globalBucketQuotaSys.Init", func() {
-			globalBucketQuotaSys.Init(newObject)
-		})
-
 		// Populate existing buckets to the etcd backend
 		if globalDNSConfig != nil {
 			// Background this operation.
