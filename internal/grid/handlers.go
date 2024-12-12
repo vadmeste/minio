eac11e1a1a4658c3002d8d7025c0fc8aeba7f27d
Anis Eleuch <vadmeste@users.noreply.github.com>
2024-12-12 06:50:57 +0100

    heal: Move CheckParts from single handler to streaming RPC (#20755)
    CheckParts call can take time to verify 10k parts of a single object in a single drive.
    To avoid an internal dealine of one minute in the single handler RPC, this commit will
    switch to streaming RPC instead.

--- internal/grid/handlers.go
+++ internal/grid/handlers.go
@@ -113,6 +113,9 @@ const (
 	HandlerRenameDataInline
 	HandlerRenameData2
 	HandlerCheckParts2
+	HandlerRenamePart
+	HandlerClearUploadID
+	HandlerCheckParts3
 
 	// Add more above here ^^^
 	// If all handlers are used, the type of Handler can be changed.
@@ -194,6 +197,9 @@ var handlerPrefixes = [handlerLast]string{
 	HandlerRenameDataInline:            storagePrefix,
 	HandlerRenameData2:                 storagePrefix,
 	HandlerCheckParts2:                 storagePrefix,
+	HandlerCheckParts3:                 storagePrefix,
+	HandlerRenamePart:                  storagePrefix,
+	HandlerClearUploadID:               peerPrefix,
 }
 
 const (
