48db6cd6da44df9db307004a5d485e2d85a2de2e
Anis Eleuch <anis@min.io>
2025-09-19 13:14:11 +0100

    Address Klaus reviews

--- cmd/delayed-context-cancel.go
+++ cmd/delayed-context-cancel.go
@@ -1,75 +1,77 @@
+// MinIO, Inc. CONFIDENTIAL
+//
+// [2014] - [2025] MinIO, Inc. All Rights Reserved.
+//
+// NOTICE:  All information contained herein is, and remains the property
+// of MinIO, Inc and its suppliers, if any.  The intellectual and technical
+// concepts contained herein are proprietary to MinIO, Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law. Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from MinIO, Inc.
+
 package cmd
 
 import (
 	"context"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-// delayedCancelContext wraps another context and delays its cancellation
+// delayedCancelContext delays cancellation after parent context is canceled
 type delayedCancelContext struct {
 	parent context.Context
 	done   chan struct{}
-	err    error
-	mu     sync.RWMutex
-	values map[interface{}]interface{}
+	err    atomic.Pointer[error]
 	delay  time.Duration
-	once   sync.Once
 }
 
-// WithDelayedCancel creates a new context that will be canceled one minute
-// after the parent context is canceled
+// WithDelayedCancel returns a context that cancels after a delay when parent cancels
 func WithDelayedCancel(parent context.Context, delay time.Duration) (context.Context, context.CancelFunc) {
 	ctx := &delayedCancelContext{
 		parent: parent,
 		done:   make(chan struct{}),
-		values: make(map[interface{}]interface{}),
 		delay:  delay,
 	}
 
-	// Set up manual cancel function
-	manualCancel := func() {
-		ctx.once.Do(func() {
-			ctx.mu.Lock()
-			ctx.err = context.Canceled
+	var once sync.Once
+	cancel := func() {
+		once.Do(func() {
+			ctx.err.Store(&context.Canceled)
 			close(ctx.done)
-			ctx.mu.Unlock()
 		})
 	}
 
-	// Start goroutine to monitor parent context
 	go func() {
 		select {
 		case <-parent.Done():
-			timer := time.NewTimer(delay)
-			defer timer.Stop()
-
-			select {
-			case <-timer.C:
-				ctx.once.Do(func() {
-					ctx.mu.Lock()
-					ctx.err = parent.Err() // Inherit the parent's error
-					close(ctx.done)
-					ctx.mu.Unlock()
-				})
-			case <-ctx.done:
-				// Context was manually canceled before the delay expired
-				return
-			}
 		case <-ctx.done:
-			// Context was manually canceled
 			return
+		}
+
+		timer := time.NewTimer(delay)
+		defer timer.Stop()
+
+		select {
+		case <-timer.C:
+			once.Do(func() {
+				parentErr := parent.Err()
+				ctx.err.Store(&parentErr)
+				close(ctx.done)
+			})
+		case <-ctx.done:
+			// Manual cancel before delay
 		}
 	}()
 
-	return ctx, manualCancel
+	return ctx, cancel
 }
 
-// Implement the Context interface
-func (c *delayedCancelContext) Deadline() (deadline time.Time, ok bool) {
-	// If parent has deadline, add one minute to account for the delay
-	if parentDeadline, hasDeadline := c.parent.Deadline(); hasDeadline {
-		return parentDeadline.Add(c.delay), true
+func (c *delayedCancelContext) Deadline() (time.Time, bool) {
+	if d, ok := c.parent.Deadline(); ok {
+		// Add delay to parent deadline to reflect actual cancellation time
+		return d.Add(c.delay), true
 	}
 	return time.Time{}, false
 }
@@ -79,26 +81,12 @@ func (c *delayedCancelContext) Done() <-chan struct{} {
 }
 
 func (c *delayedCancelContext) Err() error {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	return c.err
+	if err := c.err.Load(); err != nil {
+		return *err
+	}
+	return nil
 }
 
 func (c *delayedCancelContext) Value(key interface{}) interface{} {
-	c.mu.RLock()
-	if val, exists := c.values[key]; exists {
-		c.mu.RUnlock()
-		return val
-	}
-	c.mu.RUnlock()
-
-	// Delegate to parent for inherited values
 	return c.parent.Value(key)
-}
-
-// SetValue allows setting values on the delayed context (optional utility)
-func (c *delayedCancelContext) SetValue(key, value interface{}) {
-	c.mu.Lock()
-	c.values[key] = value
-	c.mu.Unlock()
 }
